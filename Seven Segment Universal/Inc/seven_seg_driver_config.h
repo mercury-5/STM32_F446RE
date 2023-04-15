45ae4e22eda3a552af2dbabb1cc9baeb674188e3
Soumyakanta Behera <120708699+soumya7686@users.noreply.github.com>
2023-04-15 16:33:52 +0530

    Add files via upload

---
+++ Seven Segment Universal/Inc/seven_seg_driver_config.h
@@ -0,0 +1,17 @@
+/****************************************************/
+/*  seven_seg_config.h                              */
+/*                                                  */
+/*  Created on: Apr 15, 2023                        */
+/*  Author: Soumyakanta Behera						*/
+/****************************************************/
+
+#ifndef INC_SEVEN_SEG_DRIVER_CONFIG_H_
+#define INC_SEVEN_SEG_DRIVER_CONFIG_H_
+
+#include "seven_segment_driver.h"
+
+
+extern SEVEN_SEGMENT_Config SEVEN_SEG_Parameters;
+
+
+#endif /* INC_SEVEN_SEG_DRIVER_CONFIG_H_ */
