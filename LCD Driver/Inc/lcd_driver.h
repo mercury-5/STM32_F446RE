3e2c9e487983dd9022b2283f838719b0dcbbcfc9
Soumyakanta Behera <120708699+soumya7686@users.noreply.github.com>
2023-04-19 10:17:20 +0530

    Update lcd_driver.h

--- LCD Driver/Inc/lcd_driver.h
+++ LCD Driver/Inc/lcd_driver.h
@@ -21,7 +21,7 @@ extern "C" {
 #define LCD_CLEARDISPLAY 			0x01
 #define LCD_RETURNHOME 				0x02
 #define LCD_ENTRYMODESET 			0x04
-#define LCD_DISPLAYCONTROL 		0x08
+#define LCD_DISPLAYCONTROL 			0x08
 #define LCD_CURSORSHIFT 			0x10
 #define LCD_FUNCTIONSET 			0x20
 #define LCD_SETCGRAMADDR 			0x40
@@ -36,37 +36,37 @@ extern "C" {
 /* Display On/Off */
 #define LCD_DISPLAYON 				0x04
 #define LCD_DISPLAYOFF 				0x00
-#define LCD_CURSORON 				  0x02
+#define LCD_CURSORON 				0x02
 #define LCD_CURSOROFF 				0x00
-#define LCD_BLINKON 				  0x01
-#define LCD_BLINKOFF 				  0x00
+#define LCD_BLINKON 				0x01
+#define LCD_BLINKOFF 				0x00
 
 /* Cursor Shift */
 #define LCD_DISPLAYMOVE 			0x08
 #define LCD_CURSORMOVE 				0x00
 #define LCD_MOVERIGHT 				0x04
-#define LCD_MOVELEFT 				  0x00
+#define LCD_MOVELEFT 				0x00
 
 /* Function Set */
-#define LCD_8BITMODE 				  0x10
-#define LCD_4BITMODE 			  	0x00
-#define LCD_2LINE 				  	0x08
-#define LCD_1LINE 					  0x00
-#define LCD_5x10DOTS 			  	0x04
-#define LCD_5x8DOTS 				  0x00
+#define LCD_8BITMODE 				0x10
+#define LCD_4BITMODE 				0x00
+#define LCD_2LINE 					0x08
+#define LCD_1LINE 					0x00
+#define LCD_5x10DOTS 				0x04
+#define LCD_5x8DOTS 				0x00
 
 /* Backlight */
 #define LCD_BACKLIGHT 				0x08
 #define LCD_NOBACKLIGHT 			0x00
 
 /* Enable Bit */
-#define ENABLE 						    0x04
+#define ENABLE 						0x04
 
 /* Read Write Bit */
-#define RW 							      0x00
+#define RW 							0x0
 
 /* Register Select Bit */
-#define RS 							      0x01
+#define RS 							0x01
 
 void LCD_Init(uint8_t rows);
 void LCD_Clear();
